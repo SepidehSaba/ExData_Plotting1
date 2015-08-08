6b7dd60478ea81099e17627a4932e3daa5627f7b
SepidehSaba <saba.sepideh@gmail.com>
2015-08-07 18:36:50 -0700

    R codes and PNG images

---
+++ Project R file and Png files/plot1.R
@@ -0,0 +1,11 @@
+data <- read.table("household_power_consumption.txt", sep = ";", header = T, na.strings = "?")
+data$Date <- strptime(data$Date, format= "%d/%m/%Y")
+format(data$Date, format="%Y-%m-%d")
+data <- subset(data, data$Date %in% as.POSIXlt(c('2007-02-01', '2007-02-02')))
+data$Datetime <- strptime(paste(data$Date, data$Time), "%d-%m-%Y %H:%M:%S")
+hist(data$Global_active_power, main = "Global Active Power", ylab = "Frequency", xlab = "Global Active Power (kilowatts)", col = "red",ylim = c(0, 1200), xlim = c(0, 6))
+
+
+#copy to png file
+dev.copy(png, file="plot1.png" )
+dev.off()
